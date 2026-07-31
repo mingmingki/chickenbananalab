f9ee539e3eb830e933ff94a63d48c7f783dbf907
박민기 <bagmingi@bagmingiui-MacBookPro.local>
2026-07-31 12:05:05 +0900

    fix(video): video_library_api에서 reverse 미임포트로 인한 500 에러 수정
    관리자(can_manage=True)로 동영상/쇼츠 라이브러리를 열면 edit_url/delete_url을
    reverse()로 만드는데, django.urls.reverse를 import하지 않아 NameError로
    API가 500을 반환하던 버그. from django.urls import reverse 추가.
    staff 계정으로 video_library_api를 직접 호출해 재현/수정 확인함.

--- core/views.py
+++ core/views.py
@@ -29,6 +29,7 @@ from django.db.models import Q, Count, Sum, Min
 from django.db.models.functions import TruncDate
 from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.html import strip_tags
 from django.views.decorators.http import require_POST
